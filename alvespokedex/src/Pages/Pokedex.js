42195d5622cf28e9e6bc32e6b6332fc02794d108
Gabriela Tobias <gabih_tobias@hotmail.com>
2022-07-18 17:57:57 -0300

    Inicio

---
+++ alvespokedex/src/Pages/Pokedex.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+export default function Pokedex() {
+  return (
+    <div>Pokedex</div>
+  )
+}
---
+++ alvespokedex/src/Router/Coordinator.js
@@ -0,0 +1,14 @@
+export const homeNav = (navigate) => {
+
+    navigate('/')
+}
+
+export const pokedexNav = (navigate) => {
+
+    navigate('pokedex')
+}
+
+export const detailsNav = (navigate) => {
+
+    navigate('detalhe')
+}
